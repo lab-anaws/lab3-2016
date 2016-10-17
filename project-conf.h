e092e4989778477033af3754c5cfaa14fb8c9719
Carlo Vallati <carlo.vallati@iet.unipi.it>
2016-10-17 14:35:40 +0200

    Added slides and code

---
+++ project-conf.h
@@ -0,0 +1,11 @@
+
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#undef CC2420_CONF_CHANNEL 
+#define CC2420_CONF_CHANNEL 20
+
+#undef UIP_CONF_ND6_SEND_NA
+#define UIP_CONF_ND6_SEND_NA            1
+
+#endif /* PROJECT_CONF_H_ */
